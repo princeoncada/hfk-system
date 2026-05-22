c315ae823db40a5f1950f6a20b1847a1020a0d6c
princeoncada <prince.oncada@gmail.com>
2026-05-22 10:35:46 -0400

    config: add openai to serverComponentsExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['chromadb', 'chromadb-default-embed'],
+    serverComponentsExternalPackages: [
+      'chromadb',
+      'chromadb-default-embed',
+      'openai',
+    ],
   },
 }
 export default nextConfig
